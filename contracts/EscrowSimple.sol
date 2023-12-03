591eda21be45e814efb04a9c11a778ea132cfd4f
Anya Quote <anya.quote@gmail.com>
2023-12-03 22:23:56 +0700

    feat: simple lock claim

--- contracts/EscrowSimple.sol
+++ contracts/EscrowSimple.sol
@@ -1,11 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
+
 contract EscrowSimple {
+    struct Fund {
+        address owner;
+        address payee;
+        uint256 amount;
+        uint unlockTime;
+        uint expireTime;
+        uint id;
+        bool claimed;
+    }
+
     uint256 public borrowAmount = 100;
     mapping(address => uint256) public balances;
+    Fund[] private funds;
 
     event Borrow(address borrower);
+
+    event LockFund(
+        address owner,
+        address payee,
+        uint256 amount,
+        uint unlockTime,
+        uint expireTime,
+        uint id
+    );
+
+    event Withdraw(address claimer, uint fundId, uint amount);
 
     constructor() {}
 
@@ -13,5 +37,125 @@ contract EscrowSimple {
     function borrow() external {
         balances[msg.sender] += borrowAmount;
         emit Borrow(msg.sender);
+    }
+
+    function lockFund(
+        address payee,
+        uint unlockTime,
+        uint expireTime,
+        uint amount
+    ) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        require(amount > 0, "Invalid amount");
+        require(
+            unlockTime > block.timestamp,
+            "Unlock time must be in the future"
+        );
+        require(
+            expireTime > unlockTime,
+            "Expire time must be after unlock time"
+        );
+
+        uint id = funds.length + 1;
+        balances[msg.sender] -= amount;
+        funds.push(
+            Fund({
+                owner: msg.sender,
+                payee: payee,
+                unlockTime: unlockTime,
+                expireTime: expireTime,
+                amount: amount,
+                id: id,
+                claimed: false
+            })
+        );
+
+        emit LockFund(msg.sender, payee, amount, unlockTime, expireTime, id);
+    }
+
+    // This is not working
+    function getMyClaimableFunds() external view returns (Fund[] memory) {
+        uint count = 0;
+        for (uint i = 0; i < funds.length; i++) {
+            Fund memory fund = funds[i];
+
+            if (
+                !fund.claimed &&
+                ((fund.payee == msg.sender &&
+                    fund.expireTime > block.timestamp &&
+                    fund.unlockTime < block.timestamp) ||
+                    (fund.owner == msg.sender &&
+                        fund.expireTime < block.timestamp))
+            ) {
+                count++;
+            }
+        }
+
+        Fund[] memory result = new Fund[](count);
+        uint256 j;
+        for (uint i = 0; i < funds.length; i++) {
+            Fund memory fund = funds[i];
+            if (
+                !fund.claimed &&
+                ((fund.payee == msg.sender &&
+                    fund.expireTime > block.timestamp &&
+                    fund.unlockTime < block.timestamp) ||
+                    (fund.owner == msg.sender &&
+                        fund.expireTime < block.timestamp))
+            ) {
+                result[j] = fund;
+            }
+        }
+        return result;
+    }
+
+    function withdraw(uint fundId) external {
+        require(fundId <= funds.length, "Invalid fund id");
+
+        Fund storage fund = funds[fundId - 1];
+
+        require(
+            fund.payee == msg.sender || fund.owner == msg.sender,
+            "You don't have permission to withdraw this fund"
+        );
+        require(fund.claimed == false, "This fund has already been claimed");
+
+        if (msg.sender == fund.payee) {
+            require(
+                block.timestamp > fund.unlockTime,
+                "You can not claim this fund yet"
+            );
+            require(block.timestamp < fund.expireTime, "Expired fund");
+        } else {
+            require(
+                block.timestamp > fund.expireTime,
+                "You can not withdraw this fund yet"
+            );
+        }
+
+        fund.claimed = true;
+        balances[msg.sender] += fund.amount;
+
+        emit Withdraw(msg.sender, fundId, fund.amount);
+    }
+
+    function getMyRelatedFunds() external view returns (Fund[] memory) {
+        uint count = 0;
+        for (uint i = 0; i < funds.length; i++) {
+            Fund memory fund = funds[i];
+            if ((fund.payee == msg.sender || fund.owner == msg.sender)) {
+                count++;
+            }
+        }
+
+        Fund[] memory result = new Fund[](count);
+        uint256 j;
+        for (uint i = 0; i < funds.length; i++) {
+            Fund memory fund = funds[i];
+            if ((fund.payee == msg.sender || fund.owner == msg.sender)) {
+                result[j] = fund;
+            }
+        }
+        return result;
     }
 }
--- test/EscrowSimple.ts
+++ test/EscrowSimple.ts
@@ -2,6 +2,12 @@ import { ethers } from "hardhat";
 import { before } from "mocha";
 import { EscrowSimple } from "../typechain-types";
 import { expect } from "chai";
+import { AddressLike } from "ethers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import {
+	time,
+	loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("EscrowSimple contract", () => {
 	let contract: EscrowSimple;
@@ -32,6 +38,300 @@ describe("EscrowSimple contract", () => {
 			await expect(contract.connect(borrower).borrow())
 				.to.emit(contract, "Borrow")
 				.withArgs(borrower.address);
+		});
+	});
+
+	describe("Lock fund", () => {
+		let address1: HardhatEthersSigner;
+		let balance: bigint;
+
+		let address2: HardhatEthersSigner;
+
+		before(async () => {
+			const [address, addressNext] = await ethers.getSigners();
+			address1 = address;
+			address2 = addressNext;
+		});
+
+		beforeEach(async () => {
+			balance = await contract.balances(address1.address);
+			if (balance === 0n) {
+				await contract.connect(address1).borrow();
+			}
+			balance = await contract.balances(address1.address);
+		});
+
+		it("should throw if not enough balance", () => {
+			expect(
+				contract
+					.connect(address1)
+					.lockFund(address2.address, Date.now(), Date.now(), balance + 1n)
+			).to.be.revertedWith("Not enough tokens");
+		});
+
+		it("should throw if invalid amount", () => {
+			expect(
+				contract
+					.connect(address1)
+					.lockFund(address2.address, Date.now(), Date.now(), 0)
+			).to.be.revertedWith("Invalid amount");
+		});
+
+		describe("Invalid unlock time and expire time", async () => {
+			it("should throw if unlock time in the past", () => {
+				expect(
+					contract
+						.connect(address1)
+						.lockFund(address2.address, Date.now() / 1000, Date.now(), balance)
+				).to.be.revertedWith("Unlock time must be in the future");
+			});
+
+			it("should throw if expire time is before unlock time", () => {
+				const unlockTime = new Date();
+				unlockTime.setMilliseconds(
+					unlockTime.getMilliseconds() + 1000 * 60 * 60
+				);
+				it("should throw if unlock time in the past", () => {
+					expect(
+						contract
+							.connect(address1)
+							.lockFund(
+								address2.address,
+								unlockTime.getMilliseconds(),
+								Date.now(),
+								balance
+							)
+					).to.be.revertedWith("Expire time must be after unlock time");
+				});
+			});
+		});
+
+		it("should have fund after lock", async () => {
+			const currentDate = new Date();
+
+			await contract
+				.connect(address1)
+				.lockFund(
+					address2.address,
+					Math.round((currentDate.getTime() * 1000 * 20) / 1000),
+					Math.round((currentDate.getTime() * 1000 * 60) / 1000),
+					balance
+				);
+
+			const funds = await contract.connect(address2).getMyRelatedFunds();
+			expect(funds.length).to.greaterThan(0);
+
+			const newestFund = funds.sort((a, b) => Number(b.id - a.id))[0];
+			expect(newestFund.amount).to.eq(balance);
+		});
+
+		it("should have more claimable fund after lock", async () => {
+			const currentDate = new Date();
+
+			await contract
+				.connect(address1)
+				.lockFund(
+					address2.address,
+					Math.round((currentDate.getTime() * 1000 * 20) / 1000),
+					Math.round((currentDate.getTime() * 1000 * 60 * 60) / 1000),
+					balance / 2n
+				);
+
+			const funds = await contract.connect(address2).getMyRelatedFunds();
+			expect(funds.length).to.greaterThan(0);
+
+			await contract
+				.connect(address1)
+				.lockFund(
+					address2.address,
+					Math.round((currentDate.getTime() * 1000 * 20) / 1000),
+					Math.round((currentDate.getTime() * 1000 * 60 * 60) / 1000),
+					balance / 2n
+				);
+
+			expect(
+				(await contract.connect(address2).getMyRelatedFunds()).length
+			).to.greaterThan(funds.length);
+		});
+	});
+
+	describe("Claim fund", () => {
+		let address1: HardhatEthersSigner;
+		let balance: bigint;
+
+		let address2: HardhatEthersSigner;
+
+		before(async () => {
+			const addresses = await ethers.getSigners(); // default 2
+			address1 = addresses[addresses.length - 1];
+			address2 = addresses[addresses.length - 2];
+		});
+
+		beforeEach(async () => {
+			balance = await contract.balances(address1.address);
+			if (balance === 0n) {
+				await contract.connect(address1).borrow();
+			}
+			balance = await contract.balances(address1.address);
+		});
+
+		it("should have 0 claimable funds", async () => {
+			const funds1 = await contract.connect(address1).getMyRelatedFunds();
+			expect(funds1.length).to.eq(0);
+
+			const funds2 = await contract.connect(address2).getMyRelatedFunds();
+			expect(funds2.length).to.eq(0);
+		});
+
+		describe("Payee claim", () => {
+			beforeEach(async () => {
+				balance = await contract.balances(address1.address);
+				if (balance === 0n) {
+					await contract.connect(address1).borrow();
+				}
+				balance = await contract.balances(address1.address);
+			});
+
+			it("should fail to claim if fund not unlocked", async () => {
+				await contract
+					.connect(address1)
+					.lockFund(
+						address2.address,
+						Math.round((new Date().getTime() + 1000 * 60 * 60) / 1000),
+						Math.round((new Date().getTime() + 1000 * 60 * 120) / 1000),
+						balance
+					);
+
+				const relatedFunds = await contract
+					.connect(address2)
+					.getMyRelatedFunds();
+				const claimableFunds = await contract
+					.connect(address2)
+					.getMyClaimableFunds();
+
+				expect(relatedFunds.length).to.eq(1);
+				expect(claimableFunds.length).to.eq(0);
+
+				expect(
+					contract.connect(address2).withdraw(relatedFunds[0].id)
+				).to.be.revertedWith("You can not claim this fund yet");
+			});
+
+			it("should not claimable if not have permission", async () => {
+				const addresses = await ethers.getSigners();
+
+				const relatedFunds = await contract
+					.connect(address2)
+					.getMyRelatedFunds();
+
+				expect(
+					contract
+						.connect(
+							addresses.find(
+								(address) =>
+									address.address !== address1.address &&
+									address.address !== address2.address
+							)
+						)
+						.withdraw(relatedFunds[0].id)
+				).to.be.revertedWith("You don't have permission to withdraw this fund");
+			});
+
+			it("should claimable in time", async () => {
+				const unlockTime = (await time.latest()) + 60;
+				const address1Balance = await contract.balances(address1.address);
+
+				await contract
+					.connect(address1)
+					.lockFund(address2.address, unlockTime, unlockTime + 120, balance);
+
+				await time.increaseTo(unlockTime + 1);
+
+				const claimableFunds = await contract
+					.connect(address2)
+					.getMyClaimableFunds();
+
+				const claimFund = claimableFunds[0];
+
+				const address2Balance = await contract.balances(address2.address);
+
+				await contract.connect(address2).withdraw(claimFund.id);
+
+				expect(await contract.balances(address1.address)).to.eq(
+					address1Balance - claimFund.amount
+				);
+				expect(await contract.balances(address2.address)).to.eq(
+					address2Balance + claimFund.amount
+				);
+
+				const relatedFunds = await contract
+					.connect(address2)
+					.getMyRelatedFunds();
+
+				expect(relatedFunds.find((f) => f.id === claimFund.id)?.claimed).to.eq(
+					true
+				);
+			});
+		});
+
+		describe("Payer withdraw", () => {
+			let unlockTime;
+			let expireTime;
+			let fundId: bigint;
+			before(async () => {
+				await contract.connect(address1).borrow();
+
+				unlockTime = (await time.latest()) + 60 * 60;
+				expireTime = unlockTime + 120;
+				const address1Balance = await contract.balances(address1.address);
+
+				await contract
+					.connect(address1)
+					.lockFund(address2.address, unlockTime, expireTime, address1Balance);
+
+				const relatedFunds = await contract
+					.connect(address1)
+					.getMyRelatedFunds();
+
+				fundId = relatedFunds[0].id;
+			});
+			beforeEach(async () => {
+				balance = await contract.balances(address1.address);
+				if (balance === 0n) {
+					await contract.connect(address1).borrow();
+				}
+				balance = await contract.balances(address1.address);
+			});
+
+			it("should not claimable before unlock time", async () => {
+				expect(contract.connect(address1).withdraw(fundId!)).to.be.revertedWith(
+					"You can not withdraw this fund yet"
+				);
+			});
+
+			it("should not claimable between unlock time and expire itme", async () => {
+				await time.increaseTo(unlockTime! + 1);
+
+				expect(contract.connect(address1).withdraw(fundId!)).to.be.revertedWith(
+					"You can not withdraw this fund yet"
+				);
+			});
+
+			it("should claimable after expire time", async () => {
+				await time.increaseTo(expireTime! + 1);
+
+				const relatedFunds = await contract
+					.connect(address1)
+					.getMyRelatedFunds();
+
+				await contract.connect(address1).withdraw(fundId!);
+
+				const newBalance = await contract.balances(address1.address);
+
+				expect(newBalance).to.be.eq(
+					(relatedFunds.find((f) => f.id === fundId!)?.amount || 0n) + balance
+				);
+			});
 		});
 	});
 });
