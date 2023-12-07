3526c20743138419a200ba4f41854fe70c60fda7
Anya Quote <anya.quote@gmail.com>
2023-12-07 20:02:33 +0700

    feat: allow contract withdraw by part

--- contracts/EscrowErc20.sol
+++ contracts/EscrowErc20.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract EscrowErc20 {
     address public lender;
@@ -10,22 +11,57 @@ contract EscrowErc20 {
     uint public loan;
     IERC20 public token;
     bool public claimed;
+    uint public steps;
+    uint public claimedSteps;
+    uint public lockedAt;
+    uint public claimedAmount;
 
-    function fund(IERC20 _token, address _borrower, uint _loan) external {
+    function fund(
+        IERC20 _token,
+        address _borrower,
+        uint _loan,
+        uint _steps
+    ) external {
+        require(lender == address(0), "You cant fund again");
         lender = msg.sender;
         borrower = _borrower;
         loan = _loan;
         token = _token;
         claimed = false;
+        steps = _steps;
+        claimedSteps = 0;
+        lockedAt = block.timestamp;
+        claimedAmount = 0;
 
         token.transferFrom(msg.sender, address(this), _loan);
     }
 
     function withdraw() external {
         require(msg.sender == borrower, "Only borrower can withdraw!");
-        require(!claimed, "This fund had already been claimed");
+        require(claimedSteps != steps, "this fund is empty");
 
-        token.transfer(msg.sender, loan);
-        claimed = true;
+        uint weekDiff = (block.timestamp - lockedAt) / 1 weeks;
+
+        uint releaseAmount = 0;
+
+        if (weekDiff >= steps) {
+            releaseAmount = loan - claimedAmount;
+        } else if (weekDiff < 1) {
+            releaseAmount = 0;
+        } else {
+            uint releaseStepCount = weekDiff - claimedSteps;
+            releaseAmount = ((loan) / steps) * releaseStepCount;
+        }
+
+        claimedSteps = steps < weekDiff ? steps : weekDiff;
+        claimedAmount += releaseAmount;
+
+        token.transfer(msg.sender, releaseAmount);
+    }
+
+    function calculateDayDiff() external view returns (uint) {
+        uint dayDiff = (block.timestamp - lockedAt) / 1 weeks;
+
+        return dayDiff;
     }
 }
